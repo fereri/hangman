3a5f9c65b45afdffc2117d9c15d356cff1f4edb8
David <250323761+fereri@users.noreply.github.com>
2026-07-27 17:49:11 +0200

    Word between 5 and 12 characters now gets selected only.

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
-p File.exist?("lib/10000-words.txt")
-
 words = File.open("lib/10000-words.txt")
+true_word = ""
+word_length = false
 
-word = File.readlines(words).sample.chomp
+# get word between 5 and 12 characters long
+until word_length == true do
+  word = File.readlines(words).sample.chomp
 
-p word
+  if word.length >= 5 and word.length <= 12
+    true_word = word
+    word_length = true
+  else
+    next
+  end
+end
