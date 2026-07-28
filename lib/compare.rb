7718cc261c49b054d9d8154179de2d4bd4f568a1
David <250323761+fereri@users.noreply.github.com>
2026-07-28 19:02:55 +0200

    Incorrect letters notice, added.

--- lib/compare.rb
+++ lib/compare.rb
@@ -4,9 +4,14 @@ class Compare
     split = word.split("")
     length = word.length()
     correct_word = Array.new(length, "_")
+    incorrect_letters = []
     
     until correct_word.include?("_") == false do
       input = gets.chomp
+
+      if split.include?(input) == false
+        incorrect_letters.push(input)
+      end
 
       split.each_with_index do |char, index|
         if char == input
@@ -18,6 +23,7 @@ class Compare
 
       joined = correct_word.join("")
       p joined
+      puts "The incorrect letters are: #{incorrect_letters.join("")}."
 
     end
 
